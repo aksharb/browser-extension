00c6622b8d824a61632cf63ee0f33375c388976f
Akshar Bisht <bisht.akshar@gmail.com>
2022-03-30 17:21:43 +0530

    make list items hyperlinks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 myLeads = [];
-let inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
@@ -13,7 +13,10 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `<li><a target='_blank' href=' 
+      ${myLeads[i]}'> 
+      ${myLeads[i]}
+      </a></li>`;
+    ulEl.innerHTML = listItems;
   }
-  ulEl.innerHTML = listItems;
 }
