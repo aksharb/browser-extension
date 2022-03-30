f8f6db0960ef24369e48d3437d091d75e799a0af
Akshar Bisht <bisht.akshar@gmail.com>
2022-03-30 16:49:44 +0530

    added feature to display leads enterd from input field to the page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-myLeads = ["one", "two", "three"];
+myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -6,10 +6,13 @@ const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function () {
   const inputValue = document.getElementById("input-el").value;
   myLeads.push(inputValue);
-  console.log(myLeads);
+  renderLeads();
 });
-let listItems = "";
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>";
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
 }
-ulEl.innerHTML = listItems;
